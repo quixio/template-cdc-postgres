1f8a6019872288665b87ac7a507de7406a3f827a
Steve Rosam <srosam+quix-demos@gmail.com>
2024-02-06 15:26:49 +0000

    Edited 'main.py'

--- SDF Postgres Sink/main.py
+++ SDF Postgres Sink/main.py
@@ -1,61 +1,17 @@
-from quixstreams import Application
-from quixstreams.models.serializers.quix import JsonDeserializer
-
 import os
-import psycopg2 as p2
-from psycopg2 import sql
+from quixstreams import Application
+from quixstreams.models.serializers.quix import QuixDeserializer
 
 
-# Function to insert data into the database
-def insert_data(uid, stream_id, timestamp, data):
-    # Connect to your postgres DB
-    conn = p2.connect(
-        dbname="your_dbname",
-        user="your_username",
-        password="your_password",
-        host="your_host",
-        port="your_port"
-    )
+app = Application.Quix("transformation-v1", auto_offset_reset="latest")
 
-    # Open a cursor to perform database operations
-    with conn.cursor() as cur:
-        # Prepare the INSERT statement
-        query = sql.SQL("""
-            INSERT INTO csv_data_parameter_data (uid, stream_id, "timestamp", "data")
-            VALUES (%s, %s, to_timestamp(%s), %s);
-        """)
+input_topic = app.topic(os.environ["input"], value_deserializer=QuixDeserializer())
 
-        # Execute the query
-        cur.execute(query, (uid, stream_id, timestamp, data))
+sdf = app.dataframe(input_topic)
 
-        # Commit the transaction
-        conn.commit()
+# Here put transformation logic.
 
-    # Close the connection
-    conn.close()
-
-
-def sink_to_pdb(row):
-    print(row)
-
-
-def main():
-    app = Application.Quix("transformation-v1", auto_offset_reset="latest")
-
-    input_topic = app.topic(os.environ["input"], value_deserializer=JsonDeserializer())
-
-    sdf = app.dataframe(input_topic)
-
-    # Here put transformation logic.
-    sdf = sdf.update(sink_to_pdb)
-
-    #sdf = sdf.update(lambda row: print(row))
-
-    app.run()
-
+sdf = sdf.update(lambda row: print(row))
 
 if __name__ == "__main__":
-    try:
-        main()
-    except KeyboardInterrupt:
-        print("Exiting.")
+    app.run(sdf)
