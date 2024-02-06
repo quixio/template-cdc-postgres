8b570762ea72ac63c6126bca4da00b143fa07116
Steve Rosam <srosam+quix-demos@gmail.com>
2024-02-06 15:28:12 +0000

    Edited 'main.py'

--- SDF Postgres Sink/main.py
+++ SDF Postgres Sink/main.py
@@ -1,17 +1,61 @@
-import os
 from quixstreams import Application
-from quixstreams.models.serializers.quix import QuixDeserializer
+from quixstreams.models.serializers.quix import JSONDeserializer
+
+import os
+import psycopg2 as p2
+from psycopg2 import sql
 
 
-app = Application.Quix("transformation-v1", auto_offset_reset="latest")
+# Function to insert data into the database
+def insert_data(uid, stream_id, timestamp, data):
+    # Connect to your postgres DB
+    conn = p2.connect(
+        dbname="your_dbname",
+        user="your_username",
+        password="your_password",
+        host="your_host",
+        port="your_port"
+    )
 
-input_topic = app.topic(os.environ["input"], value_deserializer=QuixDeserializer())
+    # Open a cursor to perform database operations
+    with conn.cursor() as cur:
+        # Prepare the INSERT statement
+        query = sql.SQL("""
+            INSERT INTO csv_data_parameter_data (uid, stream_id, "timestamp", "data")
+            VALUES (%s, %s, to_timestamp(%s), %s);
+        """)
 
-sdf = app.dataframe(input_topic)
+        # Execute the query
+        cur.execute(query, (uid, stream_id, timestamp, data))
 
-# Here put transformation logic.
+        # Commit the transaction
+        conn.commit()
 
-sdf = sdf.update(lambda row: print(row))
+    # Close the connection
+    conn.close()
+
+
+def sink_to_pdb(row):
+    print(row)
+
+
+def main():
+    app = Application.Quix("transformation-v1", auto_offset_reset="latest")
+
+    input_topic = app.topic(os.environ["input"], value_deserializer=JsonDeserializer())
+
+    sdf = app.dataframe(input_topic)
+
+    # Here put transformation logic.
+    sdf = sdf.update(sink_to_pdb)
+
+    #sdf = sdf.update(lambda row: print(row))
+
+    app.run()
+
 
 if __name__ == "__main__":
-    app.run(sdf)
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("Exiting.")
