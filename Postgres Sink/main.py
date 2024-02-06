91458dd157899202c5c7eaae2f84d9f19de67f8e
Steve Rosam <srosam+quix-demos@gmail.com>
2024-02-06 15:02:53 +0000

    Edited 'main.py'

--- Postgres Sink/main.py
+++ Postgres Sink/main.py
@@ -1,5 +1,7 @@
-import quixstreams as qx
-from quix_function import QuixFunction
+import os
+from quixstreams import Application, State
+from quixstreams.models.serializers.quix import QuixDeserializer, QuixTimeseriesSerializer
+
 import os
 import psycopg2
 from psycopg2 import sql
@@ -33,3 +35,20 @@ def insert_data(uid, stream_id, timestamp, data):
     # Close the connection
     conn.close()
 
+
+
+app = Application.Quix("transformation-v1", auto_offset_reset="latest")
+
+input_topic = app.topic(os.environ["input"], value_deserializer=QuixDeserializer())
+output_topic = app.topic(os.environ["output"], value_serializer=QuixTimeseriesSerializer())
+
+sdf = app.dataframe(input_topic)
+
+# Here put transformation logic.
+
+sdf = sdf.update(lambda row: print(row))
+
+sdf = sdf.to_topic(output_topic)
+
+if __name__ == "__main__":
+    app.run(sdf)
